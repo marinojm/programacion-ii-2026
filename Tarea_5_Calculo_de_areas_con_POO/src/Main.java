ff0360d1844091ebc94d4c1c91ab15280fde5c36
marinojm <marinomendoza1000@gmail.com>
2026-08-29 23:57:54 -0600

    Carga del Main

--- Tarea_5_Calculo_de_areas_con_POO/src/Main.java
+++ Tarea_5_Calculo_de_areas_con_POO/src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        Figura[] figuras = new Figura[3];
+
+        figuras[0] = new Circulo("círculo", 5.0);
+        figuras[1] = new Rectangulo("rrctángulo", 4.0, 6.0);
+        figuras[2] = new Triangulo("Trirnagulo", 3.0, 8.0);
+
+        for (Figura figura : figuras) {
+            figura.mostrarInformacion();
+            System.out.printf("Área: %.2f%n", figura.calcularArea());
+            System.out.println("---------------------------");
+        }
+    }
 }
